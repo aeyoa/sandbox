9b84c7a8c1329b6754ab83c8bd9d98413adef6de
Arseny Kogan <arsenykogan@gmail.com>
2014-04-17 18:33:27 +0400

    added gogrovy package

---
+++ gogroovy/src/C.groovy
@@ -0,0 +1,9 @@
+/**
+ * Created by arsenykogan on 16/04/14.
+ */
+class C {
+
+    def subtract = {a, b ->
+        a - b
+    }
+}
---
+++ gogroovy/src/Launcher.groovy
@@ -0,0 +1,4 @@
+/**
+ * Created by arsenykogan on 16/04/14.
+ */
+println(new C().subtract("fromabctoabs", "abc"))
